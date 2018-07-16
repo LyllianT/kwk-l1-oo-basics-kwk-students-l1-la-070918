ade2987fc1b35d55a5d8929be848347026adc061
LyllianT <lilyterrific2008@gmail.com>
2018-07-16 18:46:39 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,3 +2,13 @@
 # Add you Book class here
 class Book
   attr_accessor :title, :author, :page_count
+  def initialize(title, author, page_count)
+    @title = title
+    
+    @author = author
+    
+    @page_count = page_count
+  end
+end
+one_book = Book.new("Percy Jackson And The Sea Of Monsters", "Rick Riordan", "298")
+puts one_book.title
