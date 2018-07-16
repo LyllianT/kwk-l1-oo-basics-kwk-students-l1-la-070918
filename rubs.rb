2cded0b3e0de3f730ef4c1da610d3bd3d465d68e
LyllianT <lilyterrific2008@gmail.com>
2018-07-16 18:37:26 +0000

    automatically backed up by learn

--- rubs.rb
+++ rubs.rb
@@ -2,4 +2,12 @@ class Klossy
   def initialize(teachers, student_amount, location)
     @teachers = teachers
     
-    @student_amount = s
+    @student_amount = student_amount
+    
+    @location = location
+    
+  end
+end
+klossy_burbank = Klossy.new("Maddy & Hunter", "14 girls", "Los Angeles")
+puts klossy_burbank.teachers
+puts klossy_burbank.student_amount
